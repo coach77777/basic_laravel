80ff5c988747344152aacb502186d6e3cbe7eea4
coach77777 <108881946+coach77777@users.noreply.github.com>
2024-10-15 04:31:49 -0500

    Backend blog Category Setup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
-
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Home\AboutController;
+use App\Http\Controllers\Home\BlogCategoryController;
 use App\Http\Controllers\Home\HomeSliderController;
 use App\Http\Controllers\Home\PortfolioController;
 use Illuminate\Support\Facades\Route;
@@ -61,6 +61,21 @@ Route::controller(PortfolioController::class)->group(function () {
     Route::get('/delete/portfolio/{id}', 'DeletePortfolio')->name('delete.portfolio');
 
     Route::get('/portfolio/details/{id}', 'PortfolioDetails')->name('portfolio.details');
+
+});
+
+// Blog Category All Routes
+Route::controller(BlogCategoryController::class)->group(function () {
+    Route::get('/all/blog/category', 'AllBlogCategory')->name('all.blog.category');
+    Route::get('/add/blog/category', 'AddBlogCategory')->name('add.blog.category');
+
+    Route::post('/store/blog/category', 'StoreBlogCategory')->name('store.blog.category');
+
+    Route::get('/edit/blog/category/{id}', 'EditBlogCategory')->name('edit.blog.category');
+
+    Route::post('/update/blog/category/{id}', 'UpdateBlogCategory')->name('update.blog.category');
+
+    Route::get('/delete/blog/category/{id}', 'DeleteBlogCategory')->name('delete.blog.category');
 
 });
 
